feccd8298a2d3a7470ae2ae90a0566da7a07c3b3
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-28 16:19:50 -0500

    Changed service api to use azure api

--- Jeopardy/src/app/service/http.service.ts
+++ Jeopardy/src/app/service/http.service.ts
@@ -15,45 +15,52 @@ export class HttpService {
   // Input: category id
   // Notes: returns 5 QAs for a category
   getQuestions(category: number): Observable<any> {
-    return this.http.get<any>(`${environment.apiBaseURL}/Question/GetQuestions/${category}`);
+    return this.http.get<any>(`${environment.apiAzureURL}/Question/GetQuestions/${category}`);
+    // return this.http.get<any>(`${environment.apiBaseURL}/Question/GetQuestions/${category}`);
   }
 
   // Returns List<Team>
   // Team {ID: integer, Name: string, Score: integer}
   // Notes: the list is sorted according to score (highest to lowest)
   getSortedTeams(): Observable<any> {
-    return this.http.get<any>(`${environment.apiBaseURL}/Team/SortedTeams`);
+    return this.http.get<any>(`${environment.apiAzureURL}/Team/SortedTeams`);
+    // return this.http.get<any>(`${environment.apiBaseURL}/Team/SortedTeams`);
   }
 
   // Does not return anything
   // Input: Team {ID: integer, Name: string, Score: integer}
   // Notes: used to update team score
   updateTeam(p: Partial<any>): Observable<unknown> {
-    return this.http.put(`${environment.apiBaseURL}/Team/UpdateTeam`, p);
+    return this.http.put(`${environment.apiAzureURL}/Team/UpdateTeam`, p);
+    // return this.http.put(`${environment.apiBaseURL}/Team/UpdateTeam`, p);
   }
 
   // Returns id of created Team
   // Input: Team {ID: integer, Name: string, Score: integer}
   createTeams(p: Partial<any>): Observable<any> {
-    return this.http.post(`${environment.apiBaseURL}/Team/CreateTeams`, p);
+    return this.http.post(`${environment.apiAzureURL}/Team/CreateTeams`, p);
+    // return this.http.post(`${environment.apiBaseURL}/Team/CreateTeams`, p);
   }
 
   // Returns List<Player>
   // Player {ID: integer, Name: string, Team_ID: integer}
   // Input: team id
   getMemebers(team_id: number): Observable<any> {
-    return this.http.get<any>(`${environment.apiBaseURL}/Player/GetMembers/${team_id}`);
+    return this.http.get<any>(`${environment.apiAzureURL}/Player/GetMembers/${team_id}`);
+    // return this.http.get<any>(`${environment.apiBaseURL}/Player/GetMembers/${team_id}`);
   }
 
   // Does not return anything
   // Input: Player {ID: integer, Name: string, Team_ID: integer}
   createPlayers(p: Partial<any>): Observable<any> {
-    return this.http.post(`${environment.apiBaseURL}/Player/CreatePlayers`, p);
+    return this.http.post(`${environment.apiAzureURL}/Player/CreatePlayers`, p);
+    // return this.http.post(`${environment.apiBaseURL}/Player/CreatePlayers`, p);
   }
 
   // Returns List<Type>
   // Type {ID: integer, Category: string}
   getTypes(): Observable<any> {
-    return this.http.get<any>(`${environment.apiBaseURL}/Type/GetTypes`);
+    return this.http.get<any>(`${environment.apiAzureURL}/Category/GetCategories`);
+    // return this.http.get<any>(`${environment.apiBaseURL}/Type/GetTypes`);
   }
 }
