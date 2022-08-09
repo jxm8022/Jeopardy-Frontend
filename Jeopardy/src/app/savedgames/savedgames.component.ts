18589578f8237e41894f9e17cf9ee39b26b42efb
Jose Mendoza <mendozaj9486@gmail.com>
2022-08-09 13:47:36 -0500

    Removed hardcoded saved games and added style

--- Jeopardy/src/app/savedgames/savedgames.component.ts
+++ Jeopardy/src/app/savedgames/savedgames.component.ts
@@ -1,12 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { Answer } from '../models/Answer';
-import { Boardstate } from '../models/Boardstate';
-import { Game } from '../models/Game';
 import { GameUI } from '../models/GameUI';
-import { QA } from '../models/QA';
-import { Question } from '../models/Question';
-import { SubCategory } from '../models/SubCategory';
-import { Team } from '../models/Team';
 
 @Component({
   selector: 'app-savedgames',
@@ -26,8 +19,6 @@ export class SavedgamesComponent implements OnInit {
   ngOnInit(): void {
     if (this.savedGames.length > 0) {
       this.message = "Games retrieved successfully!";
-      this.savedGames.push(new GameUI(new Game(0, 0, 0), [new Team(0, "boop", 0)], [new SubCategory(0, "beep", 0)], [new QA(new Question(0, "", 0), new Answer(0, "", 0))], [new Boardstate(0, 0, 0, false, 0, 0)]));
-      this.savedGames.push(new GameUI(new Game(0, 0, 0), [new Team(0, "boop", 0)], [new SubCategory(0, "beep", 0)], [new QA(new Question(0, "", 0), new Answer(0, "", 0))], [new Boardstate(0, 0, 0, false, 0, 0)]));
     }
   }
 
