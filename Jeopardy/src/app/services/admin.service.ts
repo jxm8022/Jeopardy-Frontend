ac552f3fb17149d4d6984b601992eac05ca5f4b2
Jose Mendoza <mendozaj9486@gmail.com>
2022-08-24 13:48:05 -0500

    Created admin services

--- Jeopardy/src/app/services/admin.service.ts
+++ Jeopardy/src/app/services/admin.service.ts
@@ -14,7 +14,44 @@ export class AdminService {
   // Admin {ID: integer, Name: string, Password: string, Access: integer}
   // Input: username, password
   getAdmin(username: string, password: string): Observable<HttpResponse<any>> {
-    return this.http.get(`${environment.apiAzureURL}/Player/Admin/${username}/${password}`, {
+    return this.http.get(`${environment.apiAzureURL}/Admin/GetAdmin/${username}/${password}`, {
+      'observe': 'response'
+    });
+  }
+
+  // Returns List<Admin>
+  // Admin {ID: integer, Name: string, Password: string, Access: integer}
+  // Input: none
+  getAllAdmins(): Observable<HttpResponse<any>> {
+    return this.http.get(`${environment.apiAzureURL}/Admin/GetAllAdmins`, {
+      'observe': 'response'
+    });
+  }
+
+  // Returns nothing
+  // Admin {ID: integer, Name: string, Password: string, Access: integer}
+  // Input: Admin
+  createAdmin(admin: Partial<any>): Observable<HttpResponse<any>> {
+    return this.http.post(`${environment.apiAzureURL}/Admin/CreateAdmin`, admin, {
+      'observe': 'response'
+    });
+  }
+
+  // Returns nothing
+  // Admin {ID: integer, Name: string, Password: string, Access: integer}
+  // Input: Admin
+  updateAdmin(admin: Partial<any>): Observable<HttpResponse<any>> {
+    return this.http.put(`${environment.apiAzureURL}/Admin/UpdateAdmin`, admin, {
+      'observe': 'response'
+    });
+  }
+
+
+  // Returns nothing
+  // Admin {ID: integer, Name: string, Password: string, Access: integer}
+  // Input: admin_id
+  deleteAdmin(admin_id: number): Observable<HttpResponse<any>> {
+    return this.http.get(`${environment.apiAzureURL}/Admin/DeleteAdmin/${admin_id}`, {
       'observe': 'response'
     });
   }
