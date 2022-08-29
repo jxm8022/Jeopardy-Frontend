135ec288d5683e6a84c9b2671415f3367ea2753d
Jose Mendoza <mendozaj9486@gmail.com>
2022-08-29 17:44:51 -0500

    Implemented delete category

--- Jeopardy/src/app/delete_components/delete-category/delete-category.component.ts
+++ Jeopardy/src/app/delete_components/delete-category/delete-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Type } from 'src/app/models/Type';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-delete-category',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService, private router: Router) { }
 
   ngOnInit(): void {
+    this.categoryService.getTypes().subscribe(res => {
+      this.categories = res;
+      this.message = "Categories loaded successfully!";
+    });
   }
 
+  categories: Type[] = [];
+  message: string = "";
+  errorMessage: string = "";
+  update: boolean = false;
+
+  checkCategory(category: Type): boolean {
+    this.message = "";
+    if (category.subcategories.length > 0) {
+      this.errorMessage = "Cannot delete a category that has subcategories!";
+      this.message = "Delete subcategories first!";
+      return false;
+    }
+    return true;
+  }
+
+  deleteCategory(category: Type): void {
+    if (this.checkCategory(category)) {
+      if (confirm(`Delete category ${category.category.category_name}?`)) {
+        this.categoryService.deleteCategory(category.category.category_id).subscribe({
+          'next': res => {
+            if (res.status === 200) {
+              alert(`Category ${category.category.category_name} deleted!`);
+              this.router.navigate(['home/delete']);
+            }
+          }
+        });
+      }
+    }
+  }
 }
