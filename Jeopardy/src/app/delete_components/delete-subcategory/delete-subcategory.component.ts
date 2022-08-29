802253575b1da31e9823d0f282b25ee713ac3fc1
Jose Mendoza <mendozaj9486@gmail.com>
2022-08-29 17:59:53 -0500

    Implemented delete subcategory

--- Jeopardy/src/app/delete_components/delete-subcategory/delete-subcategory.component.ts
+++ Jeopardy/src/app/delete_components/delete-subcategory/delete-subcategory.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SubCategory } from 'src/app/models/SubCategory';
+import { Type } from 'src/app/models/Type';
+import { CategoryService } from 'src/app/services/category.service';
+import { QuestionService } from 'src/app/services/question.service';
 
 @Component({
   selector: 'app-delete-subcategory',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteSubcategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService, private questionService: QuestionService, private router: Router) { }
 
   ngOnInit(): void {
+    this.categoryService.getTypes().subscribe(res => {
+      this.categories = res;
+      this.message = "Categories loaded successfully!";
+    });
+  }
+
+  categories: Type[] = [];
+  message: string = "";
+  errorMessage: string = "";
+  update: boolean = false;
+
+  categoryChosen: boolean = false;
+  subcategories: SubCategory[] = [];
+  chooseCategory(category_index: number): void {
+    this.subcategories = this.categories[category_index].subcategories;
+    this.message = `Subcategories for ${this.categories[category_index].category.category_name} loaded successfully!`;
+    this.categoryChosen = true;
+  }
+
+  checkCategory(subcategory: SubCategory): any {
+    this.message = "";
+    this.errorMessage = "";
+    this.questionService.getAllQuestions(subcategory.subcategory_id).subscribe({
+      'next': res => {
+        if (res.status === 200) {
+          this.errorMessage = "Cannot delete a subcategory that has questions!";
+          this.message = "Delete questions first!";
+          return false;
+        } else {
+          return true;
+        }
+      }
+    });
+  }
+
+  deleteSubcategory(subcategory: SubCategory): void {
+    if (this.checkCategory(subcategory)) {
+      if (confirm(`Delete subcategory ${subcategory.subcategory_name}?`)) {
+        this.categoryService.deleteSubcategory(subcategory.subcategory_id).subscribe({
+          'next': res => {
+            if (res.status === 200) {
+              alert(`Subcategory ${subcategory.subcategory_name} deleted!`);
+              this.router.navigate(['home/delete']);
+            }
+          }
+        });
+      }
+    }
   }
 
 }
